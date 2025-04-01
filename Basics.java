2568f9500bd49acc0b8c5d3f1e879477490d0188
Ayush vishnoi <ayushvishnoi000@gmail.com>
2025-04-01 23:40:38 +0530

    Initial commit

---
+++ Basics.java
@@ -0,0 +1,10 @@
+public class Basics {
+    public static void main(String[] args){
+        System.out.println("HEllo World");
+        int n=113;
+        n*=10;
+        n%=10;
+        System.out.println(n);
+
+    }
+}
---
+++ inop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class inop {
+    public static void main(String[] args){
+            System.out.print("hello 1");
+            System.out.print("hello 2");
+            System.out.println("hello 3");
+            System.out.print("hello 4\n");
+            System.out.print("hello 5");
+            Scanner sc =new Scanner(System.in);
+            System.out.print("enter your name:");
+            String name = sc.nextLine();
+            System.out.println(name);
+
+
+            
+    }
+}
