f8946577f55811a1c5cf5e4458d76d7f9f6a2c0b
Ayush vishnoi <ayushvishnoi000@gmail.com>
2025-04-04 23:43:43 +0530

    Day 4
    Function-call by value, scope, shadowing, method overloading

---
+++ functions.java
@@ -0,0 +1,8 @@
+public class functions {
+    public static int add(int a,int b ){
+        return (a+b);
+    }
+    public static void main(String[] args){
+        System.out.println("The Sum of teo number:"+add(4,5));
+    }
+}
