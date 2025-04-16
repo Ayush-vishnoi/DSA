52dcca22dc33667b7c9f3fdbf0d1771e565241a3
Ayush vishnoi <ayushvishnoi000@gmail.com>
2025-04-16 23:59:24 +0530

    Day 16
    Leetcode question-(125 Palindrome String)

--- Questions/Recursion/palidroneString.java
+++ Questions/Recursion/palidroneString.java
@@ -23,3 +23,21 @@ public class palidroneString {
     }
 
 }
+
+
+// Alternate solution
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        return ispalihelp(s,0,s.length()-1);
+    }
+    public boolean ispalihelp(String s,int left,int right){
+        if(left>=right) return true;
+
+        if(s.charAt(left)!=s.charAt(right)) return false;
+
+        return ispalihelp(s,left+1,right-1);
+    }
+}
+    
