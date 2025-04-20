27318bec606271cbcce76fe5d4256b8b3d790e9c
Ayush vishnoi <ayushvishnoi000@gmail.com>
2025-04-20 23:33:27 +0530

    Day 18
    Selection sort

---
+++ Questions/Sorting/selection.java
@@ -0,0 +1,26 @@
+package Questions.Sorting;
+
+public class selection {
+    public static void main(String[] args){
+        int[] arr = {5, 2, 8, 1, 9};
+        selectionSort(arr);
+        for(int i=0;i<arr.length-1;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void selectionSort(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+                int temp=arr[min];
+                arr[min]=arr[i];
+                arr[i]=temp;
+            
+
+        }
+    }
+}
