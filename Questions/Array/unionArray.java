d92e0b299c080b13f1f6882a368cf4cf7f9157e7
Ayush vishnoi <ayushvishnoi000@gmail.com>
2025-05-06 23:12:33 +0530

    Day 26
    Union of two array using set in hashing.

---
+++ Questions/Array/unionArray.java
@@ -0,0 +1,20 @@
+package Questions.Array;
+import java.util.*;
+public class unionArray {
+    public static void main(String[] args){
+        int[] arr1={3,7,5,6,3,7,8,3};
+        int[] arr2={1,5,7,0,4,3};
+        Set<Integer> unionset =new HashSet<>();
+        for(int num:arr1){
+            unionset.add(num);
+        }
+        for(int num:arr2){
+            unionset.add(num);
+        }
+        System.out.println("Union of arrays :");
+        for(int num:unionset){
+            System.out.print(num+" ");
+        }
+
+    }
+}
