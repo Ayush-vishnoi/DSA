ee6381158b33cb234b5128b42bfeafcd18de0900
Ayush vishnoi <ayushvishnoi000@gmail.com>
2026-04-04 11:19:55 +0530

    Time: 17 ms (92.47%) | Memory: 47.2 MB (11.71%) - LeetSync

---
+++ 18-4sum/4sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        return ksum(nums,0,4,target);
+    }
+    public List<List<Integer>> ksum(int[] nums,int start,int k,long target){
+        List<List<Integer>> res=new ArrayList<>();
+        if(k==2){
+            int l=start;
+            int r=nums.length-1;
+            while(l<r){
+                long sum=(long)nums[l]+nums[r];
+                if(sum<target){
+                    l++;
+
+                }
+                else if(sum>target){
+                    r--;
+                }
+                else{
+                    res.add(new ArrayList<>(Arrays.asList(nums[l], nums[r])));
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1]) l++;
+                    while(l<r && nums[r]==nums[r+1]) r--;
+                }
+            }
+            return res;
+        }
+            for(int i=start;i<nums.length;i++){
+                if(i>start && nums[i]==nums[i-1]) continue;
+                List<List<Integer>> pair=ksum(nums,i+1,k-1,target-nums[i]);
+                for(List<Integer> trip:pair){
+                    trip.add(0,nums[i]);
+                    res.add(trip);
+                }
+            }
+            return res;
+    }
+}
